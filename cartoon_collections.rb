91810bc31de72d5923c3f85eaa65ae67bfa84a95
fortunate-database-4943 <chris@chrisphilbin.net>
2018-11-17 15:06:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def summon_captain_planet(planteer_array)
 end
 
 def long_planeteer_calls(planteer_calls_array)
-  planteer_calls_array.any? |item|
+  planteer_calls_array.any? do |item|
   item.length > 4
 end
 
