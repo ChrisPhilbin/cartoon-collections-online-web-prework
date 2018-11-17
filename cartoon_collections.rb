6fe09da1fc7416323b758c085744fbef6b036ff8
fortunate-database-4943 <chris@chrisphilbin.net>
2018-11-17 15:17:03 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -16,7 +16,13 @@ def long_planeteer_calls(planteer_calls_array)
   end
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each do |cheese|
+    if array.include?(cheese)
+      return cheese
+    else
+      return nil
+    end
+ end
 end
